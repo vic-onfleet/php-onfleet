3ad5c2b2e44181f3156f84304e4da388f2f38a6b
Victoria Denysova <victoria@onfleet.com>
2021-11-12 11:02:18 +0200

    made changes to the endpoints supported

--- php-onfleet/src/Administrator.php
+++ php-onfleet/src/Administrator.php
@@ -6,9 +6,10 @@ class Administrator extends Resource
 {
     protected $endpoints = [
         'get' => [ 'method' => 'GET', 'path' => '/admins'],
-        'update' => [ 'method' => 'PUT', 'path' => '/admins'],
+        'update' => [ 'method' => 'PUT', 'path' => '/admins/:adminId'],
         'create' => [ 'method' => 'POST', 'path' => '/admins'],
-        'deleteOne' => [ 'method' => 'DELETE', 'path' => '/admins']
+        'deleteOne' => [ 'method' => 'DELETE', 'path' => '/admins/:adminId'],
+        'matchMetadata' => ['method' => 'POST', 'path' => '/admins/metadata']
     ];
 
     public function matchMetadata($obj) {}
--- php-onfleet/src/Container.php
+++ php-onfleet/src/Container.php
@@ -5,7 +5,6 @@ namespace Onfleet;
 class Container extends Resource
 {
     protected $endpoints = [
-        'get' => ['method' => 'GET', 'path' => '/containers'],
-        'update' => ['method' => 'PUT', 'path' => '/containers']
+        'get' => ['method' => 'GET', 'path' => '/containers/:param/:containerId']
     ];
 }
